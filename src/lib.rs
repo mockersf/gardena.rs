20870176a936f1d721590fbadf518c59d6a5ed3a
François Mockers <mockersf@gmail.com>
2020-05-03 03:10:23 +0200

    :pencil: adding documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! Bindings for the Gardena API
+//!
+//! # Note on enums
+//!
+//! All enums also have an `Other(String)` variant, as the possible values are not listed in the documentation. This
+//! variant will be used when the API reports a value that I did not encounter. You are welcome to report those values
+//! so that they can be added.
+//!
+//! For devices that I don't own, those kind of fields reported by the API are left as `String`
+//!
+
+#![deny(
+    warnings,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+    missing_docs
+)]
+
 use std::cell::RefCell;
 use std::fmt;
 use std::str::FromStr;
@@ -12,7 +35,7 @@ static AUTH_URL: &str = "https://api.authentication.husqvarnagroup.dev/v1/oauth2
 static API_BASE_URL: &str = "https://api.smart.gardena.dev/v1";
 
 #[derive(Deserialize, Debug)]
-pub struct TokenInfo {
+struct TokenInfo {
     access_token: String,
     scope: String,
     expires_in: u16,
@@ -22,52 +45,74 @@ pub struct TokenInfo {
     token_type: String,
 }
 
+/// Wrapper type of a response
 #[derive(Deserialize, Debug)]
 pub struct Response<T> {
-    data: T,
+    /// Data of the response
+    pub data: T,
+    /// List of objects that are included
     #[serde(default)]
-    included: Vec<Object>,
+    pub included: Vec<Object>,
 }
 
+/// Link to another gardena object, by it's ID and type
 #[derive(Deserialize, Debug)]
 pub struct ObjectLink {
+    /// ID of the object linked to
     pub id: String,
+    /// Type of the object linked to
     #[serde(rename = "type")]
     pub ty: String,
 }
 
+/// A list of devices
 #[derive(Deserialize, Debug)]
 pub struct DeviceList {
+    /// The list of devices, as links
     pub devices: Response<Vec<ObjectLink>>,
 }
 
+/// Relations of a device
 #[derive(Deserialize, Debug)]
 pub struct DeviceRelationShips {
+    /// It's location
     pub location: Response<ObjectLink>,
+    /// The objects that compose it
     pub services: Response<Vec<ObjectLink>>,
 }
 
+/// Wrapper type for an attribute that has a timestamp
 #[derive(Deserialize, Debug)]
 pub struct Attribute<T> {
+    /// Value of the attribute
     pub value: T,
+    /// Timestamp for this value
     pub timestamp: Option<String>,
 }
 
+/// Relationship to a single device
 #[derive(Deserialize, Debug)]
 pub struct DeviceRelationShip {
+    /// Link to a device
     pub device: Response<ObjectLink>,
 }
 
+/// State of a mower
 #[derive(Debug, PartialEq, Clone, enum_utils::IterVariants, enum_utils::FromStr)]
 #[enumeration(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum MowerState {
+    /// In error (errors like [`MowerErrorCode::Lifted`](enum.MowerErrorCode.html#variant.Lifted))
     Error,
+    /// Ok
     Ok,
+    /// Warning (errors like [`MowerErrorCode::ParkedDailyLimitReached`](enum.MowerErrorCode.html#variant.ParkedDailyLimitReached))
     Warning,
+    /// Other values (see note on Enums)
     #[enumeration(skip)]
     Other(String),
 }
 impl MowerState {
+    /// Get an iterator on all variants of this enum
     pub fn to_iter() -> impl Iterator<Item = Self> + Clone {
         Self::iter()
     }
@@ -111,22 +156,34 @@ impl<'de> Deserialize<'de> for MowerState {
     }
 }
 
+/// What the mower is doing
 #[derive(Debug, PartialEq, Clone, enum_utils::IterVariants, enum_utils::FromStr)]
 #[enumeration(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum MowerActivity {
+    /// Nothing, probably with an error
     None,
+    /// Charging
     OkCharging,
+    /// Cutting according to schedule
     OkCutting,
+    /// Cutting on user override
     OkCuttingTimerOverridden,
+    /// Leaving the base
     OkLeaving,
+    /// Searching for the base
     OkSearching,
+    /// Park on user overridee
     ParkedParkSelected,
+    /// Park according to schedule
     ParkedTimer,
+    /// Paused
     Paused,
+    /// Other values (see note on Enums)
     #[enumeration(skip)]
     Other(String),
 }
 impl MowerActivity {
+    /// Get an iterator on all variants of this enum
     pub fn to_iter() -> impl Iterator<Item = Self> + Clone {
         Self::iter()
     }
@@ -176,20 +233,30 @@ impl<'de> Deserialize<'de> for MowerActivity {
     }
 }
 
+/// Error code for a mower
 #[derive(Debug, PartialEq, Clone, enum_utils::IterVariants, enum_utils::FromStr)]
 #[enumeration(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum MowerErrorCode {
+    /// Couldn't reach base as path is blocked
     ChargingStationBlocked,
+    /// Issue with front collision sensor
     CollisionSensorProblemFront,
+    /// Mower has been lifted
     Lifted,
+    /// Issue with loop wire
     NoLoopSignal,
+    /// Hatch is open
     OffHatchOpen,
+    /// Outside of it's working area
     OutsideWorkingArea,
+    /// Reached it's daily limit
     ParkedDailyLimitReached,
+    /// Other values (see note on Enums)
     #[enumeration(skip)]
     Other(String),
 }
 impl MowerErrorCode {
+    /// Get an iterator on all variants of this enum
     pub fn to_iter() -> impl Iterator<Item = Self> + Clone {
         Self::iter()
     }
@@ -239,63 +306,92 @@ impl<'de> Deserialize<'de> for MowerErrorCode {
     }
 }
 
+/// Attributes of a mower
 #[derive(Deserialize, Debug)]
 pub struct MowerAttributes {
+    /// It's state
     pub state: Attribute<MowerState>,
+    /// What it is doing
     pub activity: Attribute<MowerActivity>,
     #[serde(rename = "lastErrorCode")]
+    /// Current error code, if there is one
     pub last_error_code: Option<Attribute<MowerErrorCode>>,
     #[serde(rename = "operatingHours")]
+    /// Number of hours it's been operating
     pub operating_hours: Attribute<u16>,
 }
 
+/// Attributes of a power socket
 #[derive(Deserialize, Debug)]
 pub struct PowerSocketAttributes {
+    /// It's state
     pub state: Attribute<String>,
+    /// What it is doing
     pub activity: Attribute<String>,
+    /// Current error code, if there is one
     #[serde(rename = "lastErrorCode")]
     pub last_error_code: Option<Attribute<String>>,
+    /// For how long
     pub duration: Attribute<u16>,
 }
 
+/// Attributes of a valve
 #[derive(Deserialize, Debug)]
 pub struct ValveAttributes {
+    /// It's name
     pub name: Attribute<String>,
+    /// It's state
     pub state: Attribute<String>,
+    /// What it is doing
     pub activity: Attribute<String>,
+    /// Current error code, if there is one
     #[serde(rename = "lastErrorCode")]
     pub last_error_code: Option<Attribute<String>>,
+    /// For how long
     pub duration: Attribute<u16>,
 }
 
+/// Attributes of a valve set
 #[derive(Deserialize, Debug)]
 pub struct ValveSetAttributes {
+    /// It's state
     pub state: Attribute<String>,
+    /// Current error code, if there is one
     #[serde(rename = "lastErrorCode")]
     pub last_error_code: Option<Attribute<String>>,
 }
 
+/// Attributes of a sensor
 #[derive(Deserialize, Debug)]
 pub struct SensorAttributes {
+    /// Soil humidity
     #[serde(rename = "soilHumidity")]
     pub soil_humidity: Attribute<f32>,
+    /// Soil temperature
     #[serde(rename = "soilTemperature")]
     pub soil_temperature: Attribute<f32>,
+    /// Ambient temperature
     #[serde(rename = "ambientTemperature")]
     pub ambient_temperature: Attribute<f32>,
+    /// Light intensity
     #[serde(rename = "lightIntensity")]
     pub light_intensity: Attribute<f32>,
 }
 
+/// State of the battery of an object
 #[derive(Debug, PartialEq, Clone, enum_utils::IterVariants, enum_utils::FromStr)]
 #[enumeration(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum CommonBatteryState {
+    /// Charging
     Charging,
+    /// Full
     Ok,
+    /// Other values (see note on Enums)
     #[enumeration(skip)]
     Other(String),
 }
 impl CommonBatteryState {
+    /// Get an iterator on all variants of this enum
     pub fn to_iter() -> impl Iterator<Item = Self> + Clone {
         Self::iter()
     }
@@ -341,14 +437,18 @@ impl<'de> Deserialize<'de> for CommonBatteryState {
     }
 }
 
+/// State of the connection to the Smart Gateway
 #[derive(Debug, PartialEq, Clone, enum_utils::IterVariants, enum_utils::FromStr)]
 #[enumeration(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum CommonRfLinkState {
+    /// Online
     Online,
+    /// Other values (see note on Enums)
     #[enumeration(skip)]
     Other(String),
 }
 impl CommonRfLinkState {
+    /// Get an iterator on all variants of this enum
     pub fn to_iter() -> impl Iterator<Item = Self> + Clone {
         Self::iter()
     }
@@ -390,93 +490,146 @@ impl<'de> Deserialize<'de> for CommonRfLinkState {
     }
 }
 
+/// Common attribute to all devices
 #[derive(Deserialize, Debug)]
 pub struct CommonAttributes {
+    /// It's name
     pub name: Attribute<String>,
     #[serde(rename = "batteryLevel")]
+    /// It's battery level
     pub battery_level: Attribute<u8>,
+    /// It's battery state
     #[serde(rename = "batteryState")]
     pub battery_state: Attribute<CommonBatteryState>,
+    /// It's rf link level to the Smart Gateway
     #[serde(rename = "rfLinkLevel")]
     pub rf_link_level: Attribute<u8>,
+    /// It's rf link state to the Smart Gateway
     #[serde(rename = "rfLinkState")]
     pub rf_link_state: Attribute<CommonRfLinkState>,
+    /// It's serial number
     pub serial: Attribute<String>,
     #[serde(rename = "modelType")]
+    /// It's model type
     pub model_type: Attribute<String>,
 }
 
+/// A websocket request attributes
 #[derive(Deserialize, Debug)]
 pub struct WebsocketAttributes {
+    /// How long is the url valid
     pub validity: u16,
+    /// URL to connect to thee websocket
     pub url: String,
 }
 
+/// Attributes of a location
+#[derive(Deserialize, Debug)]
+pub struct LocationAttributes {
+    /// It's name
+    name: String,
+}
+
+/// Object that can be manipulated through the API
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Object {
+    /// A location
     #[serde(rename = "LOCATION")]
     Location {
+        /// It's ID
         id: String,
-        attributes: std::collections::HashMap<String, String>,
+        /// It's attributes
+        attributes: LocationAttributes,
+        /// It's relationship to other devices
         relationships: Option<DeviceList>,
     },
+    /// A websocket connection
     #[serde(rename = "WEBSOCKET")]
     Websocket {
+        /// It's ID
         id: String,
+        /// It's attributes
         attributes: WebsocketAttributes,
     },
+    /// A general device. This type is used to list all objects available on a device
     #[serde(rename = "DEVICE")]
     Device {
+        /// It's ID
         id: String,
+        /// It's relationship to other objects
         relationships: Option<DeviceRelationShips>,
     },
+    /// The common attributes of most objects
     #[serde(rename = "COMMON")]
     Common {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Common attributes of most objects
         attributes: CommonAttributes,
     },
+    /// Mower specific attributes
     #[serde(rename = "MOWER")]
     Mower {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Mower specific attributes
         attributes: MowerAttributes,
     },
+    /// Power socket specific attributes
     #[serde(rename = "POWER_SOCKET")]
     PowerSocket {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Power socket specific attributes
         attributes: PowerSocketAttributes,
     },
+    /// Valve specific attributes
     #[serde(rename = "VALVE")]
     Valve {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Valve specific attributes
         attributes: ValveAttributes,
     },
+    /// Valve set specific attributes
     #[serde(rename = "VALVE_SET")]
     ValveSet {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Valve set specific attributes
         attributes: ValveSetAttributes,
     },
+    /// Sensor specific attributes
     #[serde(rename = "SENSOR")]
     Sensor {
+        /// It's ID
         id: String,
+        /// The device it's related to
         relationships: Option<DeviceRelationShip>,
+        /// Sensor specific attributes
         attributes: SensorAttributes,
     },
 }
 
 #[derive(Serialize, Debug)]
-pub struct LocationIdRequested {
+struct LocationIdRequested {
     #[serde(rename = "locationId")]
     location_id: String,
 }
 
 #[derive(Serialize, Debug)]
-pub struct WebsocketRequest {
+struct WebsocketRequest {
     id: String,
     #[serde(rename = "type")]
     ty: String,
@@ -484,10 +637,12 @@ pub struct WebsocketRequest {
 }
 
 #[derive(Serialize, Debug)]
-pub struct Request<T> {
+struct Request<T> {
     data: T,
 }
 
+/// Gardena API Client
+#[derive(Debug)]
 pub struct Gardena {
     client: reqwest::Client,
     username: String,
@@ -497,6 +652,7 @@ pub struct Gardena {
 }
 
 impl Gardena {
+    /// Create a new Gardena API client
     pub fn new(username: String, password: String, application_key: String) -> Gardena {
         Gardena {
             client: reqwest::Client::new(),
@@ -507,6 +663,7 @@ impl Gardena {
         }
     }
 
+    /// login to the API, getting a new token
     pub async fn login(&self) -> Result<(), Box<dyn std::error::Error>> {
         let token_info = self
             .client
@@ -555,6 +712,7 @@ impl Gardena {
         Ok(request_builder)
     }
 
+    /// List the locations linked to the Gardena account.
     pub async fn list_locations(&self) -> Result<Vec<Object>, Box<dyn std::error::Error>> {
         Ok(self
             .add_headers(self.client.get(&format!("{}/locations", API_BASE_URL)))
@@ -566,6 +724,7 @@ impl Gardena {
             .data)
     }
 
+    /// Get details from a location, to list all its objects
     pub async fn get_location(
         &self,
         location_id: &str,
@@ -583,6 +742,7 @@ impl Gardena {
             .included)
     }
 
+    /// Get the websocket URL to get updates from a location
     pub async fn get_websocket_url(
         &self,
         location_id: &str,
@@ -617,11 +777,17 @@ impl Gardena {
             .data)
     }
 }
+
+/// websocket management using async std runtime
 #[cfg(feature = "websocket-asyncstd")]
 pub mod asyncstd {
     use futures::{future::Either, prelude::*};
     use log::warn;
 
+    /// Connect and manage the websocket using asyncstd runtime.
+    ///
+    /// The websocket is closed by Gardena every 60-ish minutes as it's token expires and the url needs to be renewed
+    /// using the get_websocket_url method
     pub async fn connect_to_websocket<F>(
         url: String,
         act: F,
@@ -629,7 +795,7 @@ pub mod asyncstd {
     where
         F: Fn(super::Object) -> (),
     {
-        let (ws_stream, _) = asyncstd_tungstenite::async_std::connect_async(url).await?;
+        let (ws_stream, _) = async_tungstenite::async_std::connect_async(url).await?;
 
         let should_break = std::sync::Mutex::new(false);
 
@@ -689,7 +855,7 @@ pub mod asyncstd {
 
             // ping future was the first future to end, send ping
             write
-                .send(asyncstd_tungstenite::tungstenite::Message::text("Ping"))
+                .send(async_tungstenite::tungstenite::Message::text("Ping"))
                 .await?;
         }
 
@@ -697,11 +863,16 @@ pub mod asyncstd {
     }
 }
 
+/// websocket management using tokio runtime
 #[cfg(feature = "websocket-tokio")]
 pub mod tokio {
     use futures::{future::Either, prelude::*};
     use log::warn;
 
+    /// Connect and manage the websocket using tokio runtime.
+    ///
+    /// The websocket is closed by Gardena every 60-ish minutes as it's token expires and the url needs to be renewed
+    /// using the get_websocket_url method
     pub async fn connect_to_websocket<F>(
         url: String,
         act: F,
@@ -709,7 +880,7 @@ pub mod tokio {
     where
         F: Fn(super::Object) -> (),
     {
-        let (ws_stream, _) = tokio_tungstenite::tokio::connect_async(url).await?;
+        let (ws_stream, _) = async_tungstenite::tokio::connect_async(url).await?;
 
         let should_break = std::sync::Mutex::new(false);
 
@@ -770,7 +941,7 @@ pub mod tokio {
 
             // ping future was the first future to end, send ping
             write
-                .send(tokio_tungstenite::tungstenite::Message::text("Ping"))
+                .send(async_tungstenite::tungstenite::Message::text("Ping"))
                 .await?;
         }
 
